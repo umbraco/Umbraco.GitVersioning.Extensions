43b5477e3347b886298fb734571b4276b6863280
Paul Johnson <pmj@umbraco.com>
2022-05-10 15:47:46 +0100

    Initial commit

---
+++ src/Umbraco.GitVersioning.Extensions/GetUmbracoBuildVersion.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Build.Framework;
+
+namespace Umbraco.GitVersioning.Extensions;
+
+public class GetUmbracoBuildVersion : Microsoft.Build.Utilities.Task
+{
+    public bool PublicRelease { get; set; }
+
+    [Required]
+    public string BuildVersionSimple { get; set; } = null!;
+
+    public string? PrereleaseVersion { get; set; }
+
+    [Required]
+    public int GitVersionHeight { get; set; }
+
+    [Required]
+    public string GitCommitIdShort { get; set; } = null!;
+
+    [Output]
+    public string AssemblyInformationalVersion { get; set; } = null!;
+
+    [Output] 
+    public string CloudBuildNumber { get; private set; } = null!;
+
+    [Output]
+    public string PackageVersion { get; private set; } = null!;
+
+    public override bool Execute()
+    {
+        if (PublicRelease)
+        {
+            AssemblyInformationalVersion = $"{BuildVersionSimple}{PrereleaseVersion}+{GitCommitIdShort}";
+            CloudBuildNumber = $"{BuildVersionSimple}{PrereleaseVersion}";
+            PackageVersion = $"{BuildVersionSimple}{PrereleaseVersion}";
+        }
+        else
+        {
+            var prefix = string.IsNullOrEmpty(PrereleaseVersion)
+                ? string.Empty
+                : PrereleaseVersion.TrimStart('-') + ".";
+
+            AssemblyInformationalVersion = $"{BuildVersionSimple}--{prefix}preview.{GitVersionHeight}+{GitCommitIdShort}";
+            CloudBuildNumber = AssemblyInformationalVersion;
+            PackageVersion = AssemblyInformationalVersion.Replace("+", ".g");
+        }
+
+        return true;
+    }
+}
---
+++ tests/Umbraco.GitVersioning.Extensions.Unit.Tests/GetUmbracoBuildVersionTests.cs
@@ -0,0 +1,94 @@
+using FluentAssertions;
+using Xunit;
+
+namespace Umbraco.GitVersioning.Extensions;
+
+public class GetUmbracoBuildVersionTests
+{
+    [Theory]
+    [InlineData(false, "1.2.3", "-alpha.1", 3, "abcd123", "1.2.3--alpha.1.preview.3+abcd123")]
+    [InlineData(false, "1.2.3", null, 3, "abcd123", "1.2.3--preview.3+abcd123")]
+    [InlineData(false, "1.2.3", "", 3, "abcd123", "1.2.3--preview.3+abcd123")]
+    [InlineData(true, "1.2.3", "-alpha.1", 3, "abcd123", "1.2.3-alpha.1+abcd123")]
+    [InlineData(true, "1.2.3", null, 3, "abcd123", "1.2.3+abcd123")]
+    [InlineData(true, "1.2.3", "", 3, "abcd123", "1.2.3+abcd123")]
+    public void Execute_ForGivenInputs_HasCorrectAssemblyInformationalVersion(
+        bool publicRelease,
+        string version,
+        string preRelease,
+        int height,
+        string sha,
+        string expected)
+    {
+        var sut = new GetUmbracoBuildVersion
+        {
+            PublicRelease = publicRelease,
+            BuildVersionSimple = version,
+            PrereleaseVersion = preRelease,
+            GitVersionHeight = height,
+            GitCommitIdShort = sha
+        };
+
+        sut.Execute();
+
+        sut.AssemblyInformationalVersion.Should().Be(expected);
+    }
+
+    [Theory]
+    [InlineData(false, "1.2.3", "-alpha.1", 3, "abcd123", "1.2.3--alpha.1.preview.3+abcd123")]
+    [InlineData(false, "1.2.3", null, 3, "abcd123", "1.2.3--preview.3+abcd123")]
+    [InlineData(false, "1.2.3", "", 3, "abcd123", "1.2.3--preview.3+abcd123")]
+    [InlineData(true, "1.2.3", "-alpha.1", 3, "abcd123", "1.2.3-alpha.1")]
+    [InlineData(true, "1.2.3", null, 3, "abcd123", "1.2.3")]
+    [InlineData(true, "1.2.3", "", 3, "abcd123", "1.2.3")]
+    public void Execute_ForGivenInputs_HasCorrectCloudBuildNumber(
+        bool publicRelease,
+        string version,
+        string preRelease,
+        int height,
+        string sha,
+        string expected)
+    {
+        var sut = new GetUmbracoBuildVersion
+        {
+            PublicRelease = publicRelease,
+            BuildVersionSimple = version,
+            PrereleaseVersion = preRelease,
+            GitVersionHeight = height,
+            GitCommitIdShort = sha
+        };
+
+        sut.Execute();
+
+        sut.CloudBuildNumber.Should().Be(expected);
+    }
+
+    [Theory]
+    [InlineData(false, "1.2.3", "-alpha.1", 3, "abcd123", "1.2.3--alpha.1.preview.3.gabcd123")]
+    [InlineData(false, "1.2.3", null, 3, "abcd123", "1.2.3--preview.3.gabcd123")]
+    [InlineData(false, "1.2.3", "", 3, "abcd123", "1.2.3--preview.3.gabcd123")]
+    [InlineData(true, "1.2.3", "-alpha.1", 3, "abcd123", "1.2.3-alpha.1")]
+    [InlineData(true, "1.2.3", null, 3, "abcd123", "1.2.3")]
+    [InlineData(true, "1.2.3", "", 3, "abcd123", "1.2.3")]
+    public void Execute_ForGivenInputs_HasCorrectPackageVersion(
+        bool publicRelease,
+        string version,
+        string preRelease,
+        int height,
+        string sha,
+        string expected)
+    {
+        var sut = new GetUmbracoBuildVersion
+        {
+            PublicRelease = publicRelease,
+            BuildVersionSimple = version,
+            PrereleaseVersion = preRelease,
+            GitVersionHeight = height,
+            GitCommitIdShort = sha
+        };
+
+        sut.Execute();
+
+        sut.PackageVersion.Should().Be(expected);
+    }
+}
