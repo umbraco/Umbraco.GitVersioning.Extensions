7899358d9ec83bbe697a7222ce56b3b0ca242e67
Paul Johnson <pmj@umbraco.com>
2022-05-20 23:52:40 +0100

    Convert to netstandard2.0 to fix for visual studio.

--- src/Umbraco.GitVersioning.Extensions/GetUmbracoBuildVersion.cs
+++ src/Umbraco.GitVersioning.Extensions/GetUmbracoBuildVersion.cs
@@ -1,50 +1,55 @@
-﻿using Microsoft.Build.Framework;
+﻿using System;
+using Microsoft.Build.Framework;
 
-namespace Umbraco.GitVersioning.Extensions;
-
-public class GetUmbracoBuildVersion : Microsoft.Build.Utilities.Task
+namespace Umbraco.GitVersioning.Extensions
 {
-    public bool PublicRelease { get; set; }
-
-    [Required]
-    public string BuildVersionSimple { get; set; } = null!;
-
-    public string? PrereleaseVersion { get; set; }
-
-    [Required]
-    public int GitVersionHeight { get; set; }
-
-    [Required]
-    public string GitCommitIdShort { get; set; } = null!;
-
-    [Output]
-    public string AssemblyInformationalVersion { get; set; } = null!;
-
-    [Output] 
-    public string CloudBuildNumber { get; private set; } = null!;
-
-    [Output]
-    public string PackageVersion { get; private set; } = null!;
-
-    public override bool Execute()
+    public class GetUmbracoBuildVersion : Microsoft.Build.Utilities.Task
     {
-        if (PublicRelease)
-        {
-            AssemblyInformationalVersion = $"{BuildVersionSimple}{PrereleaseVersion}+{GitCommitIdShort}";
-            CloudBuildNumber = $"{BuildVersionSimple}{PrereleaseVersion}";
-            PackageVersion = $"{BuildVersionSimple}{PrereleaseVersion}";
-        }
-        else
-        {
-            var prefix = string.IsNullOrEmpty(PrereleaseVersion)
-                ? string.Empty
-                : PrereleaseVersion.TrimStart('-') + ".";
+        public bool PublicRelease { get; set; }
 
-            AssemblyInformationalVersion = $"{BuildVersionSimple}--{prefix}preview.{GitVersionHeight}+{GitCommitIdShort}";
-            CloudBuildNumber = AssemblyInformationalVersion;
-            PackageVersion = AssemblyInformationalVersion.Replace("+", ".g");
-        }
+        [Required]
+        public string BuildVersionSimple { get; set; }
 
-        return true;
+        public string PrereleaseVersion { get; set; }
+
+        [Required]
+        public int GitVersionHeight { get; set; }
+
+        [Required]
+        public string GitCommitIdShort { get; set; }
+
+        [Output]
+        public string AssemblyInformationalVersion { get; set; }
+
+        [Output]
+        public string CloudBuildNumber { get; private set; }
+
+        [Output]
+        public string PackageVersion { get; private set; }
+
+        public override bool Execute()
+        {
+            if (PublicRelease)
+            {
+                AssemblyInformationalVersion = $"{BuildVersionSimple}{PrereleaseVersion}+{GitCommitIdShort}";
+                CloudBuildNumber = $"{BuildVersionSimple}{PrereleaseVersion}";
+                PackageVersion = $"{BuildVersionSimple}{PrereleaseVersion}";
+            }
+            else
+            {
+                var prefix = string.IsNullOrEmpty(PrereleaseVersion)
+                    ? string.Empty
+                    : PrereleaseVersion.TrimStart('-') + ".";
+
+                AssemblyInformationalVersion = $"{BuildVersionSimple}--{prefix}preview.{GitVersionHeight}+{GitCommitIdShort}";
+                CloudBuildNumber = AssemblyInformationalVersion;
+                PackageVersion = AssemblyInformationalVersion.Replace("+", ".g");
+            }
+
+            Console.WriteLine($"##vso[task.setvariable variable=GitAssemblyInformationalVersion;]{PackageVersion}");
+
+            return true;
+        }
     }
+
 }
